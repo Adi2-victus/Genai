31a4accc6e4032c27004a66ae6040ccf0d89c62a
Aditya Darade <adityadarade682005@gmail.com>
2026-03-18 15:41:30 +0530

    handle history automatically

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 import { GoogleGenAI } from "@google/genai";
 import dotenv from "dotenv";
+import readlinesync from "readline-sync";
 dotenv.config();
+
 const ai = new GoogleGenAI({apiKey:process.env.SECRET_API_KEY});
 
+
+
+
+//handle a history automatically
 async function main() {
-  const response = await ai.models.generateContent({
+  const chat = ai.chats.create({
     model: "gemini-3-flash-preview",
+    history: [],
     config:{
+      
       systemInstruction:`You are a coding tutor,strict rule to follow
       - you will only answer the question which is related to coding
       -dont answer anything which is not relatd to coding
       -reply rudely to user if they ask question which is not related coding 
       Ex.you dumb,only ask question related to coding`
     },
-    contents: "what is current date",
-   
-    
-     });
-  console.log(response.text);
-}
+  });
 
+  
+
+  while(true)
+  {
+    const question=readlinesync.question("Ask me Question: ");
+    //break
+    if(question=="exit")
+    {
+      break;
+    }
+    const response = await chat.sendMessage({
+      message: question,
+    });
+    console.log("Chat response:", response.text);
+  }
+
+}
 await main();
