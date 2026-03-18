fae8f398bd7570dcd0ef6f58b038327661c9a8c2
Aditya Darade <adityadarade682005@gmail.com>
2026-03-18 14:11:53 +0530

    how to find current date

--- index.js
+++ index.js
@@ -6,30 +6,13 @@ const ai = new GoogleGenAI({apiKey:process.env.SECRET_API_KEY});
 async function main() {
   const response = await ai.models.generateContent({
     model: "gemini-3-flash-preview",
-    // contents: "What is array in dsa,explain in short",
-    contents:[
-      {
-        role:"user",
-        parts:[{text:"what is my name?"}]
-      },
-      {
-        role:"model",
-        parts:[{text:"I don’t know your name yet! As an AI, I don’t have access to your personal information or identity unless you tell me"}]
-      },
-      {
-        role:"user",
-        parts:[{text:"my name is Aditya"}]
-      },
-      {
-        role:"user",
-        parts:[{text:"Nice to meet you, Aditya! How can I help you today"}]
-      },
-      {
-        role:"user",
-        parts:[{text:"what is your name?"}]
-      },
-    ]
-     })
+    config:{
+      systemInstruction:`current user name is Aditya Darade ,Today date is ${new Date()}`
+    },
+    contents: "what is current date",
+   
+    
+     });
   console.log(response.text);
 }
 
