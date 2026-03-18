fbc8eec6a4ad50af7b8e9dee2899424bb55613a2
Aditya Darade <adityadarade682005@gmail.com>
2026-03-18 14:17:44 +0530

    coding tutor

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ async function main() {
   const response = await ai.models.generateContent({
     model: "gemini-3-flash-preview",
     config:{
-      systemInstruction:`current user name is Aditya Darade ,Today date is ${new Date()}`
+      systemInstruction:`You are a coding tutor,strict rule to follow
+      - you will only answer the question which is related to coding
+      -dont answer anything which is not relatd to coding
+      -reply rudely to user if they ask question which is not related coding 
+      Ex.you dumb,only ask question related to coding`
     },
     contents: "what is current date",
    
